86b428c49a2ca6b222852a38cbe8108db3c6f02f
Wyll-exe <williamagossah@gmail.com>
2025-05-21 12:17:59 +0200

    ⬆️  gsap,GLTF 🎨 house model

--- disco/src/assets/components/Experience.jsx
+++ disco/src/assets/components/Experience.jsx
@@ -1,8 +1,15 @@
+import { OrbitControls, ScrollControls } from "@react-three/drei"
+import { Office } from "./Office"
+
 export const Experience = () => {
     return (
-        <mesh>
-            <boxGeometry />
-            <meshStandardMaterial color="#e0b3ff"/>
-        </mesh>
+        <>
+        <ambientLight intensity={1}/>
+            <OrbitControls enableZoom={false}/>
+            <ScrollControls pages={3} damping={0.25} >
+                <Office />
+            </ScrollControls>
+                
+        </>
     )
 }
